c93561995efe3b3550815f09c0468b2c36975d0f
MiguelSa <up202306861@up.pt>
2025-05-13 15:24:13 +0100

    Adicao de datafile

---
+++ datafile.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  2 21:10:57 2025
+
+@author: Miguel
+"""
+
+filename = "data/tech_support.db"
+
+tables = {
+    "Agent": "Agent",
+    "Ticket": "Ticket",
+    "Solution": "Solution",
+    "TicketAssignment": "TicketAssignment"
+}
